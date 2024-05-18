f47696f61dc5759ce384336e8d3d935d09a32582
Hà Nhật Đoan <160616308+HaNhatDoan@users.noreply.github.com>
2024-05-18 15:26:42 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+   $sever='localhost';
+   $user='root';
+   $pass='';
+   $database='quanlinoithat';
+   //kết nối database
+   $conn=new mysqli($sever,$user,$pass,$database);
+   //Kiểm trá xem database có kết nối thành công không
+   if($conn){
+        mysqli_query($conn,"SET NAMES 'utf8'");
+   }
+   else{
+        echo "Kết nối không thành công";
+   }
+?>
