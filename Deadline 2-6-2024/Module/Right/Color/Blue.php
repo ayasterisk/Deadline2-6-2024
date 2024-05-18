0431976de0faff0718c216420f659f65fc190a1b
Hà Nhật Đoan <160616308+HaNhatDoan@users.noreply.github.com>
2024-05-18 17:46:46 +0700

    Add files via upload

---
+++ Deadline 2-6-2024/Module/Right/Color/Blue.php
@@ -0,0 +1,158 @@
+<div class="head-right"><h3>SOFA</h3></div>
+    <div class="right-content-sanpham">
+        <div class="sanpham-one">
+            <img src="Image/blue1.jpg" alt="" />
+            <h1>Ghế sofa gỗ cao cấp hiện đại Ghế đôi sofa 115cm tiện ích sang trọng GSF001</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">1.543.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">1.950.000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue2.jpg" alt="" />
+            <h1>Thảm salon Infinity</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">500,000đ<sup>đ</sup></div>
+                <div class="reduce-sanpham">896.000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue3.jpg" alt="" />
+            <h1>Sàn Vinyl Kháng Khuẩn Planet</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">765,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue4.jpg" alt="" />
+            <h1>Ghế Beanbag Big Joe Milano Lenox Denim Cobalt</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">1,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2,100,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue5.jpg" alt="" />
+            <h1>Ghế Sofa Accent Ghế Bành Nhung Thanh Lịch </h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">5,865,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">6,027,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue6.jpg" alt="" />
+            <h1>Accent 18 trong Bàn Ottoman hình tròn, màu xanh da trời</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">3,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">3,378,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue7.jpg" alt="" />
+            <h1>Ghế nhung hiệu suất Modway Adept 47,5" màu xanh nửa đêm, 47,5 inch</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">2,300,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">5,029,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue8.jpg" alt="" />
+            <h1>Băng ghế cuối giường bọc nhung màu hải quân </h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">3,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">4,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue9.jpg" alt="" />
+            <h1>Khái niệm tiện lợi Bàn điều khiển Oxford có kệ, Màu xanh biển</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">3,500,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">3,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue10.jpg" alt="" />
+            <h1>Nội thất tuyệt vời Provence Dark Teal chần vải nhung hình chữ nhật Ghế dài Ottoman</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">3,000,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">4,700,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue11.jpg" alt="" />
+            <h1>Bộ 2 Đầu Giường Ecoprsio Đầu Giường Hiện Đại Có Ngăn Kéo Và Kệ Để Đồ Chân </h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">1,865,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/blue12.jpg" alt="" />
+            <h1>Bàn cuối bằng gỗ Acme Becci màu xanh mòng két</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">2,265,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2,800,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="sanpham">
+        <div class="pagination">
+          <a href="#">&laquo;</a>
+          <a href="#" class="active">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">&raquo;</a>
+        </div>
+      </div>
