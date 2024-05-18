dca631841a621358ed4a6fb2fa855fb357bf0707
Ayasterisk <ayasterisk@gmail.com>
2024-05-18 21:07:54 +0700

    Fix css

--- Deadline 2-6-2024/Module/Left/loc_sp.php
+++ Deadline 2-6-2024/Module/Left/loc_sp.php
@@ -1,73 +1,72 @@
 <div class="content_locsp">
-    <h4>Lọc Sản Phẩm</h4>
-    <p>
+    <p class="head-filter">
         <strong>
-            Theo hãng sản xuất
+            Lọc theo hãng sản xuất
         </strong>
     </p>
     <ul>
         <li>
-            <a href="index.php?xem=HomeOff&id=1"><div align="center"><strong>HomeOff</strong></div></a>
+            <a href="index.php?xem=HomeOff&id=1"><div class="filter"><strong>HomeOff</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Prohome-Moho&id=1"><div align="center"><strong>Prohome - Moho</strong></div></a>
+            <a href="index.php?xem=Prohome-Moho&id=1"><div class="filter"><strong>Prohome - Moho</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Fence-Jazz&id=1"><div align="center"><strong>Fence - Jazz</strong></div></a>
+            <a href="index.php?xem=Fence-Jazz&id=1"><div class="filter"><strong>Fence - Jazz</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Penny-Ahura&id=1"><div align="center"><strong>Penny - Ahura</strong></div></a>
+            <a href="index.php?xem=Penny-Ahura&id=1"><div class="filter"><strong>Penny - Ahura</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=AnotherBrand&id=1"><div align="center"><strong>Các hãng khác</strong></div></a>
+            <a href="index.php?xem=AnotherBrand&id=1"><div class="filter"><strong>Các hãng khác</strong></div></a>
         </li>
     </ul>
-    <p>
+    <p class="head-filter">
         <strong>
-            Theo màu sắc
+            Lọc theo màu sắc
         </strong>
     </p>
     <ul>
         <li>
-            <a href="index.php?xem=White&id=1"><div align="center"><strong>Trắng</strong></div></a>
+            <a href="index.php?xem=White&id=1"><div class="filter"><strong>Trắng</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Gray&id=1"><div align="center"><strong>Đen</strong></div></a>
+            <a href="index.php?xem=Gray&id=1"><div class="filter"><strong>Đen</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Brown&id=1"><div align="center"><strong>Nâu</strong></div></a>
+            <a href="index.php?xem=Brown&id=1"><div class="filter"><strong>Nâu</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Blue&id=1"><div align="center"><strong>Xanh</strong></div></a>
+            <a href="index.php?xem=Blue&id=1"><div class="filter"><strong>Xanh</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=AnotherColor&id=1"><div align="center"><strong>Các màu khác</strong></div></a>
+            <a href="index.php?xem=AnotherColor&id=1"><div class="filter"><strong>Các màu khác</strong></div></a>
         </li>
     </ul>
-    <p>
+    <p class="head-filter">
         <strong>
-            Theo loại sản phẩm
+            Lọc theo loại sản phẩm
         </strong>
     </p>
     <ul>
     <ul>
         <li>
-            <a href="index.php?xem=Sofa&id=1"><div align="center"><strong>Sofa</strong></div></a>
+            <a href="index.php?xem=Sofa&id=1"><div class="filter"><strong>Sofa</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Table&id=1"><div align="center"><strong>Bàn</strong></div></a>
+            <a href="index.php?xem=Table&id=1"><div class="filter"><strong>Bàn</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Bed&id=1"><div align="center"><strong>Giường</strong></div></a>
+            <a href="index.php?xem=Bed&id=1"><div class="filter"><strong>Giường</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=Chair&id=1"><div align="center"><strong>Ghế</strong></div></a>
+            <a href="index.php?xem=Chair&id=1"><div class="filter"><strong>Ghế</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=ArmChair&id=1"><div align="center"><strong>Arm Chair</strong></div></a>
+            <a href="index.php?xem=ArmChair&id=1"><div class="filter"><strong>Arm Chair</strong></div></a>
         </li>
         <li>
-            <a href="index.php?xem=AnotherType&id=1"><div align="center"><strong>Các loại khác</strong></div></a>
+            <a href="index.php?xem=AnotherType&id=1"><div class="filter"><strong>Các loại khác</strong></div></a>
         </li>
     </ul>
     </ul>
