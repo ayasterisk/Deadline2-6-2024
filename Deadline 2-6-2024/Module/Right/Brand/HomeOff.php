96b18a6e3a4bddf0234cd6d8a9c67d2c69297ac0
Ayasterisk <ayasterisk@gmail.com>
2024-05-19 14:33:13 +0700

    .

--- Deadline 2-6-2024/Module/Right/Brand/HomeOff.php
+++ Deadline 2-6-2024/Module/Right/Brand/HomeOff.php
@@ -5,7 +5,7 @@
             <img src="Image/tu-trung-bay-go-tu-nhien-ktb68127-01.jpg" alt="anh" width="267px" height="250px"/>
             <h1>Tủ trang trí gỗ</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">4,950,000<sup>đ</sup></div>
+                <div class="cost-sanpham">4.950.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -16,8 +16,8 @@
             <img src="Image/tu-giay-go-tu-nhien-KG68064-01a.jpg" alt="anh" width="267px" height="250px" />
             <h1>Tủ giày kết hợp kệ trang trí gỗ</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">3,780,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">4,200,000<sup>đ</sup></div>
+                <div class="cost-sanpham">3.780.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">4.200.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -28,7 +28,7 @@
             <img src="Image/tu-giay-go-cao-su-2-cua-2-ngan-keo-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Tủ để giày gỗ cao su</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">2,950,000<sup>đ</sup></div>
+                <div class="cost-sanpham">2.950.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -39,8 +39,8 @@
             <img src="Image/ke-sach-go-cao-su-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Kệ sách đứng</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">2,322,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">2,580,000<sup>đ</sup></div>
+                <div class="cost-sanpham">2.322.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2.580.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -51,8 +51,8 @@
             <img src="Image/ke-sach-6-tang-go-cao-su-0.jpg" alt="anh" width="267px" height="250px" />
             <h1>Kệ sách 6 tầng</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">1,960,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">2,450,000<sup>đ</sup></div>
+                <div class="cost-sanpham">1.960.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2.450.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -63,7 +63,7 @@
             <img src="Image/ghe-an-boc-nem-chan-sat-cao-cap-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Ghế ăn bọc nệm Si chân sắt sỡn tĩnh điện GAK135</h1>
             <div class="pri-sanpham">
-            <div class="cost-sanpham">1,650,000<sup>đ</sup></div>
+            <div class="cost-sanpham">1.650.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -74,8 +74,8 @@
             <img src="Image/giuong-tang-go-khung-sat-GT011-03.jpg" alt="anh" width="267px" height="250px" />
             <h1>Giường tầng 140x200cm gỗ cao su khung sắt GT011</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">10,972,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">11,550,000<sup>đ</sup></div>
+                <div class="cost-sanpham">10.972.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">11.550.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -86,8 +86,8 @@
             <img src="Image/ke-nha-bep-da-nang-khung-sat-go-cao-su-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Kệ bếp đa năng 60x40x115cm gỗ cao su KB68048</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">1,800,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">2,000,000<sup>đ</sup></div>
+                <div class="cost-sanpham">1.800.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2.000.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -98,8 +98,8 @@
             <img src="Image/ke-tu-dat-goc-tuong-chan-sat-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Tủ kệ góc tường gỗ cao su</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">3,825,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">4,250,000<sup>đ</sup></div>
+                <div class="cost-sanpham">3.825.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">4.250.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -110,7 +110,7 @@
             <img src="Image/gia-treo-do-phong-tam-sat-son-tinh-dien-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Kệ để đồ phòng tắm</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">2,600,000<sup>đ</sup></div>
+                <div class="cost-sanpham">2.600.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -121,8 +121,8 @@
             <img src="Image/ghe-xoay-van-phong-chan-sat-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Ghế xoay lưng thấp nệm bọc da HOGVP162</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">738,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">820,000<sup>đ</sup></div>
+                <div class="cost-sanpham">738.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">820.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -133,7 +133,7 @@
             <img src="Image/gia-treo-do-khung-sat-co-gio-de-do-ban-1.jpg" alt="anh" width="267px" height="250px" />
             <h1>Giá để đồ phòng tắm khung sắt</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">1,850,000<sup>đ</sup></div>
+                <div class="cost-sanpham">1.850.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
--- Deadline 2-6-2024/Module/Right/Brand/Prohome-Moho.php
+++ Deadline 2-6-2024/Module/Right/Brand/Prohome-Moho.php
@@ -17,8 +17,8 @@
             <img src="Image/pro_nau_noi_that_moho_giuong_ngu_go_tram_vline_1m8_1_5211e7d2a3da44f58070de291c446400_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Giường Ngủ Gỗ Tràm MOHO VLINE 601</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">7,990,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">10,990,000<sup>đ</sup></div>
+                <div class="cost-sanpham">7.990.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">10.990.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -29,8 +29,8 @@
             <img src="Image/pro_nau_noi_that_moho_ghe_sofa_go_vline_dem_be_1_810c604bcf444eb095ef1546816221bc_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Ghế Sofa Gỗ Tràm Tự Nhiên MOHO VLINE 601</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">7,990,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">11,490,000<sup>đ</sup></div>
+                <div class="cost-sanpham">7.990.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">11.490.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -41,8 +41,8 @@
             <img src="Image/pro_mau_tu_nhien_ghe_an_go_cao_su_tu_nhien_nexo_5_c8e38359019b4df8b647f8b749286bfa_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Ghế Ăn Gỗ Cao Su Tự Nhiên MOHO NEXO 601</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">1,390,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">1,990,000<sup>đ</sup></div>
+                <div class="cost-sanpham">1.390.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">1.990.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -53,8 +53,8 @@
             <img src="Image/pro_nau_noi_that_moho_tu_dau_giuong_vline_801_1_23cfecbb74a14c43918abdff9a03b485_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Tủ Đầu Giường Gỗ MOHO VLINE 801</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">1,990,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">2,490,000<sup>đ</sup></div>
+                <div class="cost-sanpham">1.990.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2.490.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -65,8 +65,8 @@
             <img src="Image/pro_nau_noi_that_moho_ghe_sofa_go_moho_hobro_5_c7649b8758c647bb9c7e7e3cfcfcf36d_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Ghế Sofa Gỗ Cao Su Tự Nhiên MOHO HOBRO 601</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">12,590,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">15,990,000<sup>đ</sup></div>
+                <div class="cost-sanpham">12.590.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">15.990.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -77,8 +77,8 @@
             <img src="Image/i_that_moho_giuo_ngu_malaga_302_1m2_1_49fc192023254e1591849c7f359b6d06_9fabf682494143569447754a6cfe688c_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Giường Ngủ Gỗ Tràm MOHO MALAGA 302 Nhiều Kích Thước</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">4,890,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">5,990,000<sup>đ</sup></div>
+                <div class="cost-sanpham">4.890.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">5.990.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -89,8 +89,8 @@
             <img src="Image/pro_nau_noi_that_moho_tu_ke_tivi_vline_1_d5fa18e6f13c49a9955e337a072ba7a7_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Tủ Kệ Tivi Gỗ Tràm MOHO VLINE 301</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">3,190,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">4,990,000<sup>đ</sup></div>
+                <div class="cost-sanpham">3.190.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">4.990.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -101,8 +101,8 @@
             <img src="Image/pro_mau_tu_nhien_noi_that_moho_ban_lam_viec_vline_1_92060b73c393469181d9d24218c38851_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Bàn Làm Việc Gỗ MOHO VLINE 601 Màu Tự Nhiên</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">1,690,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">2,990,000<sup>đ</sup></div>
+                <div class="cost-sanpham">1.690.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">2.990.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -113,8 +113,8 @@
             <img src="Image/pro_mau_tu_nhien_noi_that_moho_ke_sach_oslo_902_1_3eab0d05eb284b87acde51e4f6b75c3c_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Kệ Gỗ – Kệ Sách MOHO OSLO 902</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">3,780,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">4,200,000<sup>đ</sup></div>
+                <div class="cost-sanpham">3.780.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">4.200.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -125,8 +125,8 @@
             <img src="Image/pro_trang_noi_that_moho_ghe_van_phong_riga_1_92865499928b49a191bbf2d0e0c91f6f_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Ghế Xoay Văn Phòng Tay Gập Thông Minh MOHO RIGA 701</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">1,290,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">1,690,000<sup>đ</sup></div>
+                <div class="cost-sanpham">1.290.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">1.690.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -137,8 +137,8 @@
             <img src="Image/pro_nau_noi_that_moho_ban_an_go_vlne_1_02c484a7c1914fd18e85938012442389_master.jpg" alt="anh" width="267px" height="250px" />
             <h1>Bàn Ăn Gỗ Cao Su Tự Nhiên MOHO VLINE 601 1m6</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">3,790,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">4,590,000<sup>đ</sup></div>
+                <div class="cost-sanpham">3.790.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">4.590.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
