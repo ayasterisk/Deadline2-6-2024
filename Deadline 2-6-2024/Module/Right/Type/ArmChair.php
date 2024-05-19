7d7ae56f42c1d444fe36513d6875899edd9cefc8
Depressed-Boy <nguyenlamtrinh69@gmail.com>
2024-05-19 17:04:12 +0700

    sofa

---
+++ Deadline 2-6-2024/Module/Right/Type/ArmChair.php
@@ -0,0 +1,159 @@
+
+<div class="head-right"><h3>ArmChair</h3></div>
+    <div class="right-content-sanpham">
+        <div class="sanpham-one">
+            <img src="Image/500-bench-soul-d.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Bench Soul</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">6.750.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">3.375.000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
+                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="sanpham">
+        <div class="pagination">
+          <a href="#">&laquo;</a>
+          <a href="#" class="active">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">&raquo;</a>
+        </div>
+      </div>
--- Deadline 2-6-2024/Module/Right/Type/Sofa.php
+++ Deadline 2-6-2024/Module/Right/Type/Sofa.php
@@ -2,7 +2,31 @@
     <div class="head-right"><h3>SOFA</h3></div>
     <div class="right-content-sanpham">
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
+            <img src="Image/cfcaf591fc6de9ef6e6be3adde6e9e9c.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Ghế sofa gỗ cao cấp hiện đại Ghế đôi sofa 115cm tiện ích sang trọng GSF001</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">1.543.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">1.950.000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/pro_nau_noi_that_moho_ghe_sofa_go_vline_dem_be_1_810c604bcf444eb095ef1546816221bc_master.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Ghế Sofa Gỗ Tràm Tự Nhiên MOHO VLINE 601</h1>
+            <div class="pri-sanpham">
+                <div class="cost-sanpham">7.990.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">11.490.000<sup>đ</sup></div>
+            </div>
+            <div class="buy-sanpham">
+                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
+                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
+            </div>
+        </div>
+        <div class="sanpham-one">
+            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="anh" width="267px" height="250px" />
             <h1>Sofa Coastal góc phải vải vàng</h1>
             <div class="pri-sanpham">
                 <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
@@ -14,11 +38,11 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/Sofa-Rumba-vai-VACT11303-600x400.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Rumba vải VACT11303</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">15.920.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">19.900.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -26,11 +50,11 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/upload_610cb93bd17a44769b7b6783a893c3c4_master.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa. da. băng 1. Chocolate. Elegance</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">38.500.000<sup>đ</sup></div>
+                <div class="reduce-sanpham"></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -38,11 +62,11 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/1087_Graceful_3_2.jpg" alt="anh" width="267px" height="250px" />
+            <h1>GRACEFUL 3-MS - SOFA BĂNG 3</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">19.610.000<sup>đ</sup></div>
+                <div class="reduce-sanpham"></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -50,11 +74,11 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/pro_nau_noi_that_moho_ghe_sofa_go_moho_hobro_5_c7649b8758c647bb9c7e7e3cfcfcf36d_master (1).jpg" alt="anh" width="267px" height="250px" />
+            <h1>Ghế Sofa Gỗ Cao Su Tự Nhiên MOHO HOBRO 601</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">12.590.000<sup>đ</sup></div>
+                <div class="reduce-sanpham">15.990.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -62,11 +86,11 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/sofa-dubai-2.5-cho-da-pu-nau-hien-dai-100915.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Dubai 2,5 chỗ hiện đại da Simili Be36</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">18.660.000<sup>đ</sup></div>
+               
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -74,11 +98,10 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/sofa-3-cho-hung-king-768x511.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa Hung King 3 chỗ + Gối VACT3231</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">25.900.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -86,11 +109,10 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/sofe-2-cho-tet-vai-vact10499.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa 2 chỗ Tết vải vact10499</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">12.900.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -98,11 +120,10 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/sofa-2-cho-tet-vai-vact10504.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa 2 chỗ Tết vải vact10504</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">12.900.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -110,35 +131,10 @@
             </div>
         </div>
         <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
+            <img src="Image/sofa-osaka-3-cho-310189-5.jpg" alt="anh" width="267px" height="250px" />
+            <h1>Sofa 3 chỗ Osaka mẫu 1 vải 65</h1>
             <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
-            </div>
-            <div class="buy-sanpham">
-                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
-                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
-            </div>
-        </div>
-        <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
-            <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
-            </div>
-            <div class="buy-sanpham">
-                <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
-                <div class="wm-sanpham"><a href="#">Chi tiết</a></div>
-            </div>
-        </div>
-        <div class="sanpham-one">
-            <img src="Image/Sofa-Coastal-GP-vai-vang-rs-3-600x400.jpg" alt="" />
-            <h1>Sofa Coastal góc phải vải vàng</h1>
-            <div class="pri-sanpham">
-                <div class="cost-sanpham">38,165,000<sup>đ</sup></div>
-                <div class="reduce-sanpham">44,900,000<sup>đ</sup></div>
+                <div class="cost-sanpham">28.380.000<sup>đ</sup></div>
             </div>
             <div class="buy-sanpham">
                 <div class="add-to-cart"><a href="#">Thêm vào giỏ</a></div>
@@ -148,12 +144,12 @@
     </div>
     <div class="sanpham">
         <div class="pagination">
-          <a href="#">&laquo;</a>
-          <a href="#" class="active">1</a>
-          <a href="#">2</a>
-          <a href="#">3</a>
-          <a href="#">4</a>
-          <a href="#">5</a>
-          <a href="#">&raquo;</a>
+          <a href="index.php?xem=Sofa&id=1">&laquo;</a>
+          <a href="index.php?xem=Sofa&id=1" class="active">1</a>
+          <a href="index.php?xem=Sofa&id=2">2</a>
+          <a href="index.php?xem=Sofa&id=3">3</a>
+          <a href="index.php?xem=Sofa&id=4">4</a>
+          <a href="index.php?xem=Sofa&id=5">5</a>
+          <a href="index.php?xem=Sofa&id=2">&raquo;</a>
         </div>
       </div>
