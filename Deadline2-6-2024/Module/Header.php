218379f7e75996139e113ce887dd6be7811b2492
Depressed-Boy <nguyenlamtrinh69@gmail.com>
2024-06-08 09:09:17 +0700

    fix css header
    ok

--- Deadline2-6-2024/Module/Header.php
+++ Deadline2-6-2024/Module/Header.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <!--------------------- header --------------------->
-    <header>
+    <header id="initial-header">
       <div class="header-top-wrapper">
         <div class="header-top">
           <div class="header-top-customer">
@@ -93,5 +93,104 @@ session_start();
           </div>
         </div>
       </div>
+
+      <script>
+      window.onscroll = function() {
+    var initialHeader = document.getElementById("initial-header");
+    var stickyHeader = document.getElementById("sticky-header");
+    var stickyPoint = initialHeader.offsetHeight;
+
+    if (window.pageYOffset > stickyPoint) {
+        stickyHeader.style.top = "0";
+    } else {
+        stickyHeader.style.top = "-100px";
+    }
+};
+
+    </script>
     </header>
+
+
+    <header id="sticky-header">
+
+      <div class="header-bottom-wrapper">
+        <div class="header-bottom">
+          <div class="header-bottom-toggle">
+            <a href="#"
+              ><i class="fa-solid fa-bars" style="color: black"></i
+            ></a>
+          </div>
+          <div class="header-bottom-logo">
+            <a href="#"><img src="Image/Future furniture.png" alt=""/></a>
+          </div>
+          <div class="header-bottom-nvbar">
+            <ul>
+              <li>
+                <a href="#"
+                  >SOFA
+                  
+                </a>
+              </li>
+              <li>
+                <a href="#"
+                  >PHÒNG KHÁCH
+                  
+                </a>
+              </li>
+
+              <li>
+                <a href="#"
+                  >PHÒNG ĂN
+                  
+                </a>
+              </li>
+              <li>
+                <a href="#"
+                  >PHÒNG NGỦ
+                  
+                </a>
+              </li>
+              <li>
+                <a href="#"
+                  >PHÒNG TẮM
+                  
+                </a>
+              </li>
+              <li><a href="#">TRANG TRÍ</a></li>
+            </ul>
+          </div>
+          <div class="header-bottom-search">
+            <div class="search-icon" id="searchIcon">
+              <i class="fa-solid fa-magnifying-glass" style="color: black"></i>
+            </div>
+            <!-- Search icon, you can use any icon or an image -->
+            <div class="search-box" id="searchBox">
+              <input type="text" placeholder="Tìm kiếm..." />
+            </div>
+          </div>
+          
+          <div class="header-bottom-cart">
+            <a href="#"
+              ><i class="fa-solid fa-cart-shopping" style="color: black"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+      <script>
+      window.onscroll = function() {
+    var initialHeader = document.getElementById("initial-header");
+    var stickyHeader = document.getElementById("sticky-header");
+    var stickyPoint = initialHeader.offsetHeight;
+
+    if (window.pageYOffset > stickyPoint) {
+        stickyHeader.style.top = "0";
+    } else {
+        stickyHeader.style.top = "-100px";
+    }
+};
+
+    </script>
+      
+    </header>
+    
     <!--------------------  Header ---------------------->
