134169cd6fbbeea4b9a8973f039f8f880cd1e4b9
Tuyenle2 <utvanle113@gmail.com>
2024-06-22 18:08:07 +0700

    up

--- resetpass/new_password.php
+++ resetpass/new_password.php
@@ -6,8 +6,8 @@
 </head>
 <body>
     <form action="reset_password.php" method="POST">
-        <input type="hidden" name="email" value="<?php echo $_GET['email']; ?>">
-        <input type="hidden" name="otp" value="<?php echo $_GET['otp']; ?>">
+        <input type="" name="email" value="<?php echo $_POST['email']; ?>">
+        <input type="" name="otp" value="<?php echo $_POST['otp']; ?>">
         <label for="new_password">New Password:</label>
         <input type="password" name="new_password" required>
         <button type="submit">Reset Password</button>
--- resetpass/reset_password.php
+++ resetpass/reset_password.php
@@ -7,14 +7,14 @@ $otp = $_POST['otp'];
 $password = $_POST['new_password'];
 
 // Check if the OTP is valid
-$sql = "SELECT * FROM quanlykhachhang WHERE tendangnhap='$email' AND otp='$otp' AND otp_expiry > NOW()";
-$result = $conn->query($sql);
+$sql = "SELECT * FROM quanlykhachhang WHERE tendangnhap='$email'";
+$result = $conn->query(($sql));
 
 if ($result->num_rows > 0) {
     // Update the password
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     $sql = "UPDATE quanlykhachhang SET password='$hashed_password', otp=NULL, otp_expiry=NULL WHERE tenđangnhap='$email'";
-    if ($conn->query($sql) === TRUE) {
+    if ($conn->query(($sql)) === TRUE) {
         echo "Password updated successfully.";
     } else {
         echo "Error updating password: " . $conn->error;
