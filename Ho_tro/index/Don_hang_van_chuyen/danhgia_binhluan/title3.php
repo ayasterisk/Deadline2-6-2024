a5b018191c3740df204a69afdf9ab827b0e07a91
datk44b <144500520+datk44b@users.noreply.github.com>
2024-06-25 06:10:11 +0700

    Update title3.php

---
+++ Ho_tro/index/Don_hang_van_chuyen/danhgia_binhluan/title3.php
@@ -0,0 +1,32 @@
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 20px;
+        }
+        h2 {
+            color: #333;
+        }
+        p {
+            color: #666;
+        }
+    </style>
+    <h2>Các câu hỏi thường gặp về Đánh giá sản phẩm</h2>
+
+<ul>
+    <li><strong>1. Làm sao để đánh giá sản phẩm sau khi nhận được hàng?</strong><br>
+    </li>
+    <li><strong>2. Tôi nhận được bao nhiêu Shopee Xu cho mỗi đánh giá sản phẩm thành công?</strong><br>
+    </li>
+    <li><strong>3. Tôi có thể xóa/chỉnh sửa đánh giá/bình luận của mình trên sản phẩm không?</strong><br>
+    </li>
+    <li><strong>4. Như thế nào là 1 bài viết đánh giá để có thể nhận được xu?</strong><br>
+    </li>
+    <li><strong>5. Số Xu nhận được từ Đánh giá sản phẩm có bị mất đi không?</strong><br>
+    </li>
+    <li><strong>6. Tại sao đánh giá sản phẩm của tôi không hiển thị?</strong><br>
+    </li>
+    <li><strong>7. Tại sao tôi đã nhận được hàng nhưng không đánh giá sản phẩm được?</strong><br>
+        Bạn có thể đánh giá mỗi sản phẩm trong 1 đơn hàng 1 lần duy nhất ngay sau khi đơn hàng đã chuyển sang mục ‘Đã giao’ hoặc ngay sau khi bạn bấm ‘Đã nhận được hàng’.
+    </li>
+</ul>
