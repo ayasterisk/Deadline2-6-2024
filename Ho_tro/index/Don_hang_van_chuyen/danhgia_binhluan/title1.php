1f25efc384d72e1051fbe26ec15a564166635aeb
datk44b <144500520+datk44b@users.noreply.github.com>
2024-06-25 06:08:00 +0700

    Update title1.php

---
+++ Ho_tro/index/Don_hang_van_chuyen/danhgia_binhluan/title1.php
@@ -0,0 +1,16 @@
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 20px;
+        }
+        h2 {
+            color: #333;
+        }
+        p {
+            color: #666;
+        }
+    </style>
+    <h2>Tôi có thể xoá/chỉnh sửa đánh giá sản phẩm của mình trên FFShop không?</h2>
+    <p>Bạn không thể xóa, nhưng có thể chỉnh sửa đánh giá sản phẩm của mình trên FFShop một lần duy nhất trong vòng 30 ngày kể từ ngày gửi đánh giá đó lần đầu tiên (nếu sản phẩm đó vẫn còn được đăng tải).</p>
+    <p>Bạn đã vượt quá thời hạn chỉnh sửa đánh giá sản phẩm trên FFShop.</p>
