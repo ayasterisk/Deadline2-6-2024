a505e68780652b6cf8e1dcaf6e70eb632b05b452
datk44b <144500520+datk44b@users.noreply.github.com>
2024-06-25 06:08:37 +0700

    Update title2.php

---
+++ Ho_tro/index/Don_hang_van_chuyen/danhgia_binhluan/title2.php
@@ -0,0 +1,21 @@
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 20px;
+        }
+        h2 {
+            color: #333;
+        }
+        p {
+            color: #666;
+        }
+    </style>
+
+
+<h2>Tại Sao Đánh Giá Sản Phẩm Của Tôi Bị Xóa / Không Hiển Thị?</h2>
+
+<ul>
+    <li>Đánh giá không liên quan đến sản phẩm của Người mua</li>
+    <li>Các đánh giá thô lỗ, khiếm nhã hoặc các lý do khác không phù hợp với tiêu chuẩn cộng đồng</li>
+    <li>Các chỉnh sửa đánh giá sau 30 ngày (kể từ ngày gửi đánh giá lần đầu tiên) có thể sẽ không được lưu lại &amp; hiển thị</li>
