57669b141ec128e831bdf611f38ff34d18936533
Depressed-Boy <nguyenlamtrinh69@gmail.com>
2024-06-24 21:57:25 +0700

    trang gioi thieu

--- Layout/Header.php
+++ Layout/Header.php
@@ -9,7 +9,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/Deadline2-6-2024/connect.php';
         <div class="header-top">
           <div class="header-top-customer">
             <p><i class="fa-solid fa-phone-volume"></i> Hotline: 0123456</p>
-            <a href="#">Giới thiệu</a>
+            <a href="introduction.php">Giới thiệu</a>
             <a href="/Deadline2-6-2024/Ho_tro/index/Hotro.php">Chăm sóc khách hàng</a>
           </div>
           <div class="header-top-login">
@@ -72,6 +72,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/Deadline2-6-2024/connect.php';
 
           <div class="header-bottom-search">
             <input type="text" id="search" placeholder="Search for products...">
+            <i class="fa-solid fa-magnifying-glass search-icon"></i>
             <div id="results"></div>
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script src="./vendors/search.js"></script>
@@ -171,13 +172,13 @@ include $_SERVER['DOCUMENT_ROOT'] . '/Deadline2-6-2024/connect.php';
 
     <header id="sticky-header">
 
-      <div class="header-bottom-wrapper">
+    <div class="header-bottom-wrapper">
         <div class="header-bottom">
           <div class="header-bottom-logo">
             <a href="/Deadline2-6-2024/index.php"><img src="/Deadline2-6-2024/Image/Future furniture.png" alt=""/></a>
           </div>
           <div class="header-bottom-nvbar">
-          <ul>
+            <ul>
               <li>
                 <a href="/Deadline2-6-2024/San_pham/index.php?xem=allproduct&id=1"
                   >SẢN PHẨM
@@ -212,19 +213,101 @@ include $_SERVER['DOCUMENT_ROOT'] . '/Deadline2-6-2024/connect.php';
               <li><a href="/Deadline2-6-2024/San_pham/index.php?xem=Decoration&id=1">TRANG TRÍ</a></li>
             </ul>
           </div>
-          
+
           <div class="header-bottom-search">
             <input type="text" id="search" placeholder="Search for products...">
+            <i class="fa-solid fa-magnifying-glass search-icon"></i>
             <div id="results"></div>
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script src="./vendors/search.js"></script>
           </div>
           
           <div class="header-bottom-cart">
-            <a href="#"
+            
+            <a href="#main-cart"
               ><i class="fa-solid fa-cart-shopping" style="color: black"></i>
-              <span>0</span>
-          </a>
+              <span> 
+                <?php if(isset($_SESSION['glb_count'])){ 
+                 echo $_SESSION['glb_count'];}
+                 else{
+                  echo 0;
+                 } ?>
+              </span>
+            </a>
+            <div class="main-cart" id="main-cart">
+              <div class="cart-wrapper">
+                <div class="scroll-cart">
+                <a href="#" class="close-cart">&times;</a>
+                <form action="">
+                <h2>CART</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Sản phẩm</th>
+                    <th>Giá</th>
+                    <th>Giá khuyến mãi</th>
+                    <th>SL</th>
+                    <th>Chọn<nav></nav></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  if (!empty($_SESSION['cart'])) {
+                    $product = mysqli_query($conn, "SELECT *FROM chitietsanpham WHERE ID IN (" . implode(",", array_keys($_SESSION['cart'])) . ")");
+                  }
+                  if(isset($product)){
+                    $count = 0;
+                  while ($row = mysqli_fetch_array($product)) {
+                   
+                    $total = 0;
+                     $giamgia = ceil((($row['gia'] - $row['giakhuyenmai']) / $row['gia']) * 100);
+                  
+                                
+                                    if (isset($_SESSION['selected_voucher'])) {
+                                        $tienvoucher = (int)($_SESSION['selected_voucher']);
+                                    }
+                                    if ($giamgia == 100) {
+                                        $total += ($row['gia'] * $_SESSION['cart'][$row['ID']]);
+                                    } else {
+                                        $total += ($row['giakhuyenmai'] * $_SESSION['cart'][$row['ID']]);
+                                    }
+                                 $count++;
+                                 
+                  ?>
+                  <tr>
+                    <td style="display: flex; align-items: center;" ><img style="width: 70px" src="<?= $row['linkanhchitiet'] ?>" alt=""><?= $row['ten_sp'] ?></td>
+                    <td><p><span><?= number_format($row['gia'],0,"",",") ?></span><sup>đ</sup></p></td>
+                    <td><p><span><?= number_format($row['giakhuyenmai'],0,"",",")?></span><sup>đ</sup></p></td>
+                    <td><input style="width: 30px; outline: none;" type="number" value="<?= $_SESSION['cart'][$row['ID']] ?>" min="1" max="100"></td>
+                    <td style="cursor: pointer;">Xóa</td>
+                  </tr>
+                  
+                  <?php
+                  }
+                  $_SESSION['glb_count'] = $count;
+                }
+                  else{?>
+                    <tr>
+                     <td></td>
+                     <td></td>
+                     <td></td>
+                     <td></td>
+                     <td></td>
+                    </tr>
+                    <?php
+                  }
+                  ?>
+                </tbody>
+              </table>
+              <div style="text-align: right;" class="price-total">
+                <?php if(isset($total)){ ?>
+                <p style="font-weight: bold;">Tổng tiền:<span><?php echo number_format($total,0,"",",");  ?></span><sup>đ</sup></p>
+                <?php } ?>
+              </div>
+            </form>
+                </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
---
+++ introduction.php
@@ -0,0 +1,96 @@
+<?php
+    include "Layout/Header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Về Chúng Tôi - Thiết Kế Nội Thất</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="vendors/introduction.css">
+    <link rel="stylesheet" href="vendors/styles.css">
+</head>
+<body>
+    <section class="gioi-thieu">
+        <div class="container">
+            <h1>Về Chúng Tôi</h1>
+            <p>Chào mừng đến với trang web thiết kế nội thất của chúng tôi. Chúng tôi đam mê tạo ra những không gian đẹp và tiện nghi cho khách hàng của mình.</p>
+        </div>
+    </section>
+
+    <section class="dich-vu">
+        <div class="container">
+            <h2>Dịch Vụ Của Chúng Tôi</h2>
+            <div class="service-cards">
+                <div class="card">
+                    <i class="fa-solid fa-paint-roller"></i>
+                    <h3>Thiết Kế Nội Thất</h3>
+                    <p>Chúng tôi cung cấp dịch vụ thiết kế nội thất toàn diện, phù hợp với phong cách và sở thích của bạn.</p>
+                </div>
+                <div class="card">
+                    <i class="fa-solid fa-ruler-combined"></i>
+                    <h3>Tư Vấn Trang Trí</h3>
+                    <p>Chúng tôi sẽ giúp bạn trang trí không gian sống theo cách đẹp nhất và hiệu quả nhất.</p>
+                </div>
+                <div class="card">
+                    <i class="fa-solid fa-couch"></i>
+                    <h3>Cung Cấp Nội Thất</h3>
+                    <p>Chúng tôi cung cấp những món đồ nội thất cao cấp, mang lại sự sang trọng cho ngôi nhà của bạn.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="du-an">
+        <div class="container">
+            <h2>Dự Án Nổi Bật</h2>
+            <div class="project-gallery">
+                <div class="project">
+                    <img src="https://ankhoadesign.com.vn/wp-content/uploads/2023/12/thiet-ke-noi-that-boi-cong-ty-xhome-sai-gon.jpg" alt="Dự Án 1">
+                    <h3>Căn Hộ Cao Cấp</h3>
+                </div>
+                <div class="project">
+                    <img src="https://ankhoadesign.com.vn/wp-content/uploads/2024/05/thiet-ke-biet-thu-bat-dau-tu-viec-thu-thap-thong-tin-va-phan-tich-nhu-cau-cua-gia-chu.jpg" alt="Dự Án 2">
+                    <h3>Biệt Thự Sang Trọng</h3>
+                </div>
+                <div class="project">
+                    <img src="https://ankhoadesign.com.vn/wp-content/uploads/2024/01/thiet-ke-noi-that-phong-khach-nha-pho-hien-dai.jpg" alt="Dự Án 3">
+                    <h3>Nhà Phố Hiện Đại</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="doi-ngu">
+        <div class="container">
+            <h2>Đội Ngũ Của Chúng Tôi</h2>
+            <div class="team-members">
+                <div class="member">
+                    <img src="https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/428387287_1761423091025777_3335438595251109984_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=QQLYKRybex8Q7kNvgH59ldm&_nc_ht=scontent.fhan14-3.fna&oh=00_AYA9b_OA5VS0keN7xezzOOGu5uQVSq71GTQql417c7-vYA&oe=667F63AF" alt="Thành viên 1">
+                    <h3>Hà Nhật Đoan</h3>
+                    <p>Chuyên gia Thiết Kế Nội Thất</p>
+                </div>
+                <div class="member">
+                    <img src="https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/405430057_882187523396737_6387194397573843992_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=3tiiZqZV_BYQ7kNvgFCqlo9&_nc_ht=scontent.fhan14-3.fna&oh=00_AYANVxlvmdfZKyXHPNGeR09mLQBxTMZ6RFR3G6ePOQZAUQ&oe=667F6DCC" alt="Thành viên 2">
+                    <h3>Nguyễn Lâm Trình</h3>
+                    <p>Nhà Trang Trí</p>
+                </div>
+                <div class="member">
+                    <img src="Image/anhkhanh.PNG" alt="Thành viên 3">
+                    <h3>Nguyễn Duy Khánh</h3>
+                    <p>Chuyên gia Cung Cấp Nội Thất</p>
+                </div>
+                
+            </div>
+            cùng các cộng sự:
+            <h3>-Lê Xuân Tuyên</h3>
+                <h3>-Lê Quốc Đạt</h3>
+        </div>
+    </section>
+    <?php
+    include "Layout/Footer.php";
+?>
+</body>
+</html>
